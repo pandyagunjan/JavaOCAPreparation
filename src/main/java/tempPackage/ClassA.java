2704e122a3d25ad535471c37a2448f0d5069adc5
Gunjan Pandya <gunjanpandya@santoshs-air.lan>
2021-03-10 09:20:29 -0500

    changes

---
+++ src/main/java/tempPackage/ClassA.java
@@ -0,0 +1,4 @@
+package tempPackage;
+
+public class ClassA {
+}
---
+++ src/main/java/tempPackage/ClassB.java
@@ -0,0 +1,12 @@
+package tempPackage;
+
+import tempPackage.ClassA;
+
+public class ClassB {
+
+    public static void main(String[] args) {
+        ClassA a ;
+        System.out.println("Got it!");
+    }
+
+}
